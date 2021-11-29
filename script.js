a860a618cd18715d4163c6ac7cd0a3a3d89826b9
hamzaishfaq7 <hamzaishfaq980@gmail.com>
2021-11-28 23:58:40 -0500

    Added comments and explained code. Also cleaned up for presentation.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// THIS IS THE CODE TO MAKE THE RESPONSIVE HAMBURGER MENU WORK FOR SMALL SCREENS/MOBILE.
+
+
 const toggle = document.getElementsByClassName('toggle')[0]
 const navbarLinks = document.getElementsByClassName('navbarLinks')[0]
 
